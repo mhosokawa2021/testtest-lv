d79d411ae7b07dd995e0b6dbd6d137cfff5f51ce
mh <mhosokawa2017@gmail.com>
2022-03-06 22:50:18 +0900

    component made comit

---
+++ resources/views/components/tweet-card.blade.php
@@ -0,0 +1,11 @@
+<div class="card card-body shadow-2 mb-2">
+     <div class="d-flex justify-content-between">
+         <p>
+             <span class="font-weight-bold mr-2">サンプル名</span>
+             <span style="font-size: 0.8rem;">{{ $tweet->created_at }}</span>
+         </p>
+     </div>
+     <p class="card-text">
+         {{ $tweet->message }}
+     </p>
+ </div> 
--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -19,17 +19,7 @@
             <div class="col-12 col-sm-12 col-md-10 col-lg-8">
                     <!-- ぼやき表示用のカード 開始 -->
                     @foreach($tweets as $tweet)
-                         <div class="card card-body shadow-2 mb-2">
-                             <div class="d-flex justify-content-between">
-                                 <p>
-                                     <span class="font-weight-bold mr-2">サンプル名</span>
-                                     <span style="font-size: 0.8rem;">{{ $tweet->created_at }}</span>
-                                 </p>
-                             </div>
-                             <p class="card-text">
-                                 {{ $tweet->message }}
-                             </p>
-                         </div>
+                        <x-tweet-card :tweet="$tweet" />
                     @endforeach
             </div>
         </div>
