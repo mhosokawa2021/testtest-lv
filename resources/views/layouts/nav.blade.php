4f32584eced3f81c33c2fed59d2c48134c02bfe7
mh <mhosokawa2017@gmail.com>
2022-04-08 01:34:11 +0900

    0408 commit

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-wrap justify-between items-center mx-auto">
   <div class="flex items-center">
     <a href="/" class="flex items-center w-60">
-      <img src="https://placehold.jp/50x50.png" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo">
+      <img src="/img/logo.png" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo">
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Plzpic</span>
     </a>
       <form method="GET" action="{{ route('dashboard.search') }}">
